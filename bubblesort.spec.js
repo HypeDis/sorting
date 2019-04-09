5e65f77219a2adf12d8633a309b8260e36a42d2c
Mark Bae <markbae@Marks-MacBook-Pro.local>
2019-04-08 23:40:25 -0400

    messing around with test specs

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,22 +1,25 @@
 describe('Bubble Sort', () => {
   let testArray;
   beforeEach(() => {
-    testArray = [2, 1, 3];
+    // testArray = [2, 1, 3];
     spyOn(window, 'swap').and.callThrough();
     spyOn(window, 'compare').and.callThrough();
   });
-  it('should handle an empty array', () => {
-    expect(bubbleSort([])).toEqual([]);
-  });
-  it('should sort an array', () => {
-    expect(bubbleSort(testArray)).toEqual([1, 2, 3]);
+  describe('sorting arrays', () => {
+    it('should handle an empty array', () => {
+      expect(bubbleSort([])).toEqual([]);
+    });
+    it('should sort an array', () => {
+      expect(bubbleSort([2, 1, 3])).toEqual([1, 2, 3]);
+      expect(bubbleSort([5, 2, 65, 48, 13])).toEqual([2, 5, 13, 48, 65]);
+    });
   });
   it('should compare 4 times', () => {
-    bubbleSort(testArray);
+    bubbleSort([2, 1, 3]);
     expect(compare.calls.count()).toEqual(4);
   });
   it('should swap 1 time', () => {
-    bubbleSort(testArray);
+    bubbleSort([2, 1, 3]);
     expect(swap.calls.count()).toEqual(1);
   });
 });
